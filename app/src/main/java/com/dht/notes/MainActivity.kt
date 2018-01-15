a8e1b4dab6a71b922042b5761d390f3d617e306f
daihaitao <958471597@qq.com>
2018-01-15 22:55:22 +0800

    init

---
+++ app/src/main/java/com/dht/notes/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dht.notes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 学习笔记
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
