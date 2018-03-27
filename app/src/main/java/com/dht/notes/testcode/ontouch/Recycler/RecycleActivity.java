f7fe9be74af78173594ce5a3dbe516c3f4a727da
daihaitao <daihaitao@wanwujieshu.net>
2018-03-27 17:57:42 +0800

    interview

---
+++ app/src/main/java/com/dht/notes/testcode/ontouch/Recycler/RecycleActivity.java
@@ -0,0 +1,41 @@
+package com.dht.notes.testcode.ontouch.Recycler;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.dht.notes.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by dai on 2018/3/27.
+ */
+
+public class RecycleActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycle)
+    RecyclerView recycle;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_activity_recycle);
+        ButterKnife.bind(this);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            list.add(i);
+        }
+        MyAdapter myAdapter = new MyAdapter() ;
+        myAdapter.setList(list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplication(),LinearLayoutManager.VERTICAL,false);
+        recycle.setLayoutManager(layoutManager);
+        recycle.setAdapter(myAdapter);
+    }
+}
--- app/src/main/java/com/dht/notes/testcode/ontouch/TestActivity.java
+++ app/src/main/java/com/dht/notes/testcode/ontouch/TestActivity.java
@@ -1,11 +1,8 @@
 package com.dht.notes.testcode.ontouch;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,8 +10,10 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.dht.notes.MainActivity;
 import com.dht.notes.R;
+import com.dht.notes.testcode.ontouch.Recycler.RecycleActivity;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -42,27 +41,27 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Log.d(TAG, "onCreate() returned: " + Thread.currentThread().getId());
-        System.out.println("android.os.Process.myPid()   = " + android.os.Process.myPid()  );
-        Log.d(TAG, "Looper.getMainLooper().getThread() " + Looper.getMainLooper().getThread().getId());
-
-        Handler handler = new Handler(){
-            @Override
-            public void handleMessage(Message msg) {
-                super.handleMessage(msg);
-            }
-        };
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d(TAG, "run: ");
-
-                System.out.println("android.os.Process.myPid()   = " + android.os.Process.myPid()  );
-            }
-        });
-        thread.start();
-        HandlerThread handlerThread = new HandlerThread("hkh");
-        Log.d(TAG, " thread.getId() returned: " + thread.getId());
+//        Log.d(TAG, "onCreate() returned: " + Thread.currentThread().getId());
+//        System.out.println("android.os.Process.myPid()   = " + android.os.Process.myPid());
+//        Log.d(TAG, "Looper.getMainLooper().getThread() " + Looper.getMainLooper().getThread().getId());
+//
+//        Handler handler = new Handler() {
+//            @Override
+//            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+//            }
+//        };
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d(TAG, "run: ");
+//
+//                System.out.println("android.os.Process.myPid()   = " + android.os.Process.myPid());
+//            }
+//        });
+//        thread.start();
+//        HandlerThread handlerThread = new HandlerThread("hkh");
+//        Log.d(TAG, " thread.getId() returned: " + thread.getId());
 
 
 //        Log.i(TAG, "onCreate: ");
@@ -81,29 +80,91 @@ public class TestActivity extends AppCompatActivity {
 //                Toast.makeText(TestActivity.this, "点击" + count[0]++ + "次数", Toast.LENGTH_SHORT).show();
 //            }
 //        });
+        final ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            arrayList.add(i);
+        }
         test1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "onClick: test1");
-                Intent intent = new Intent(TestActivity.this, MainActivity.class);
-                startActivity(intent);
 
+                Task task = new Task();
+                task.execute(arrayList);
 
             }
         });
-//        test2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.i(TAG, "onClick: test2");
-//            }
-//        });
-//
+        test2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick: test2");
+                Intent intent = new Intent(TestActivity.this, RecycleActivity.class);
+                startActivity(intent);
+            }
+        });
+
 //        Handler handler = new Handler(){
 //            @Override
 //            public void handleMessage(Message msg) {
 //                super.handleMessage(msg);
 //            }
 //        } ;
+
+
+    }
+
+    class Task extends AsyncTask<ArrayList<Integer>, Integer, Integer> {
+
+
+
+        @Override
+        protected Integer doInBackground(ArrayList<Integer>[] arrayLists) {
+            Log.d(TAG, "doInBackground() returned: " + arrayLists[0]);
+            for (int i = 0; i < arrayLists[0].size(); i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                publishProgress(i);
+            }
+            return arrayLists.length;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d(TAG, "onPreExecute: ");
+            test2.setText("初始化....");
+        }
+
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.d(TAG, "onProgressUpdate() returned: " + values[0]) ;
+            test2.setText("数据更新 " + values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            Log.d(TAG, "onPostExecute() returned: " + integer);
+            test2.setText("数据更新 " + integer);
+        }
+
+        @Override
+        protected void onCancelled(Integer integer) {
+            super.onCancelled(integer);
+            Log.d(TAG, "onCancelled: " + integer);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            Log.d(TAG, "onCancelled: ");
+        }
     }
 
     @Override
