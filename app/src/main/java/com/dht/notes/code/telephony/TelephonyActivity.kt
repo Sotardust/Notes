bb27b25fbdfa247fcdca8232bbee603ecf57849b
代海涛 <haitao.dai@enerjoy.life>
2021-07-30 16:27:22 +0800

    添加数据测试协程

--- app/src/main/java/com/dht/notes/code/telephony/TelephonyActivity.kt
+++ app/src/main/java/com/dht/notes/code/telephony/TelephonyActivity.kt
@@ -16,15 +16,23 @@ import android.telephony.CellInfoWcdma
 import android.telephony.SubscriptionInfo
 import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
+import android.util.Log
 import androidx.annotation.RequiresApi
 import com.dht.notes.R
 import kotlinx.android.synthetic.main.activity_telephony.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  *  created by Administrator on 2020/9/8 17:20
  */
 
 class TelephonyActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telephony)
@@ -32,8 +40,51 @@ class TelephonyActivity : Activity() {
         phoneBtn.setOnClickListener {
             info.text = getInfo()
         }
+
+        GlobalScope.launch {
+            val result2 = getInfo5000()
+
+            Log.d(Companion.TAG, "onCreate: ")
+
+            val result = getInfo3000()
+
+            Log.d(TAG, "onCreate() called  result = ${result},result2 = $result2")
+        }
     }
 
+
+    private suspend fun getInfo3000(): String = suspendCoroutine {
+        Log.d(TAG, "getInfo3000: 111111111111")
+        runBlocking {
+            try {
+                delay(timeMillis = 3000)
+
+                it.resume("123 30000")
+            } catch (e: Exception) {
+                e.printStackTrace()
+                it.resume("123")
+            }
+        }
+
+        Log.d(TAG, "getInfo3000: 3333333333333")
+
+    }
+
+
+    private suspend fun getInfo5000(): String = suspendCoroutine {
+        Log.d(TAG, "getInfo5000: 111111111")
+        runBlocking {
+            try {
+                delay(timeMillis = 5000)
+                it.resume("345 50000")
+            } catch (e: Exception) {
+                e.printStackTrace()
+                it.resume("345")
+
+            }
+        }
+        Log.d(TAG, "getInfo5000: 33333333333")
+    }
 
     @SuppressLint("MissingPermission", "NewApi")
     private fun getInfo(): String {
@@ -110,5 +161,9 @@ class TelephonyActivity : Activity() {
 
         else -> "其他网络"
 
+    }
+
+    companion object {
+        private const val TAG = "dht123"
     }
 }
