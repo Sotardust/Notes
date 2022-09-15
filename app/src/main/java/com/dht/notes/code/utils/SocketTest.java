a41265a35e4a989f1aaef777c134b9f662715ade
haitao.dai <haitao.dai@enerjoy.life>
2022-09-15 19:54:32 +0800

    删除对应代码

--- app/src/main/java/com/dht/notes/code/utils/SocketTest.java
+++ app/src/main/java/com/dht/notes/code/utils/SocketTest.java
@@ -24,7 +24,7 @@ public class SocketTest {
 
                 Thread.sleep(1000);
                 br.write(" i =" + i);
-                Log.d(TAG, "main() called with: args = [" + args + "]");
+                Log.d(TAG, "main() called 22with: args = [" + args + "]");
                 br.newLine();
 //                br.flush();
                 System.out.println("发送数据 i 111= " + i + socket.isConnected());
